2851ba75bbf4af7b8f2135cf30352a88e1c2cbea
HannesRosenthal <Hannes.Rosenthal@deutschebahn.com>
2020-11-17 22:52:06 +0100

    cube is reaaady

--- app/src/main/java/com/example/pregamer/Dreimann.java
+++ app/src/main/java/com/example/pregamer/Dreimann.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class Dreimann extends AppCompatActivity {
 
@@ -23,7 +24,31 @@ public class Dreimann extends AppCompatActivity {
                 left_cube = (int) (Math.random()*6+1);
                 right_cube = (int) (Math.random()*6+1);
 
+                ImageView leftCubeImage = (ImageView) findViewById(R.id.leftCubeImage);
+                ImageView rightCubeImage = (ImageView) findViewById(R.id.rightCubeImage);
+                leftCubeImage.setImageResource(R.drawable.cube1);
 
+                switch (left_cube) {
+                    case 1: leftCubeImage.setImageResource(R.drawable.cube1); break;
+                    case 2: leftCubeImage.setImageResource(R.drawable.cube2); break;
+                    case 3: leftCubeImage.setImageResource(R.drawable.cube3); break;
+                    case 4: leftCubeImage.setImageResource(R.drawable.cube4); break;
+                    case 5: leftCubeImage.setImageResource(R.drawable.cube5); break;
+                    case 6: leftCubeImage.setImageResource(R.drawable.cube6); break;
+                }
+                double leftRotation = Math.random()*720 -360;
+                leftCubeImage.setRotation((float) leftRotation);
+
+                switch (right_cube) {
+                    case 1: rightCubeImage.setImageResource(R.drawable.cube1); break;
+                    case 2: rightCubeImage.setImageResource(R.drawable.cube2); break;
+                    case 3: rightCubeImage.setImageResource(R.drawable.cube3); break;
+                    case 4: rightCubeImage.setImageResource(R.drawable.cube4); break;
+                    case 5: rightCubeImage.setImageResource(R.drawable.cube5); break;
+                    case 6: rightCubeImage.setImageResource(R.drawable.cube6); break;
+                }
+                double rightRotation = Math.random()*720 -360;
+                rightCubeImage.setRotation((float) rightRotation);
             }
         });
     }
